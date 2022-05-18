1b895915fc96d8b5226efa3e6c684f3bc47ace7d
Ignacio Yz <ignacioyz@gmail.com>
2022-05-18 11:16:52 -0400

    commit tree fd8f1babac457e8eab6a1999933d9caa00fd32bb

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
-// Write your solution here!
+// // Write your solution here!
+// let cats =['Milo', 'Otis', 'Garfield']
+
+// function destructivelyAppendCat(name){
+//     cats.push(name)
+// }
+
+// function destructivelyPrependCat(name){
+//     cats.unshift(name)
+// }
+
+// function destructivelyRemoveLastCat(name){
+//     cats.pop(name)
+// }
+
+// function destructivelyRemoveFirstCat(name){
+//     cats.shift(name)
+// }
+
+// function appendCat(name){
+//    let newCats = [...cats, name]
+//    return newCats
+// }
+
+// function prependCat(name){
+//     let newCats = [name, ...cats]
+//     return newCats
+//  }
+
+//  function removeLastCat(){
+//      return cats.slice(0, cats.length-1)
+//  }
+
+
+// function removeFirstCat(){
+//      return cats.slice(1)
+// }
+
+
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph')
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop(-1)
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift(1)
+}
+
+function appendCat(name) {
+    let copyOfCats = [...cats, "Broom"]
+    return copyOfCats
+}
+
+function prependCat(name) {
+    let copyOfCats = ["Arnold", ...cats]
+    return copyOfCats
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1)
+}
+
+function removeFirstCat(){
+     return cats.slice(1)
+}
